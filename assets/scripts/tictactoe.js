3bc3f8f0c020de9129024ab3ee55a8deb66a5dee
Pwl Webdev <pwl.webdev@gmail.com>
2016-10-03 21:15:13 +0200

    Moves completed

--- assets/scripts/tictactoe.js
+++ assets/scripts/tictactoe.js
@@ -2,7 +2,19 @@ $(document).ready(function(){
 	console.log("ready");
 	var board = initBoard();
 	render(board);
+	start(board);
+});
 
+var xMove = true;
+
+var reset = function(board){
+	xMove = true;
+	clearBoard(board);
+	render(board);
+	start(board);
+}
+
+var start = function(board){
 	$('.cell').click(function(event){
 		console.log(event);
 		makeMove(event, board);
@@ -15,14 +27,23 @@ $(document).ready(function(){
 		.mouseleave(function(event){
 			hoverOut(event, board);
 		});
-});
+}
 
-var xMove = true;
+
 
 var initBoard = function(){
 	b = [[" "," "," "],[" "," "," "],[" "," "," "]];
 	return b;
 }
+var clearBoard = function(board){
+	for(var i=0; i < 3; i++){
+		for(var j=0; j < 3; j++){
+			board[i][j] = " ";
+		}
+	}
+	return board;
+}
+
 var render = function(board){
 	$('.container').empty();
 	for(var i=0; i < 3; i++){
@@ -46,6 +67,7 @@ var makeMove = function(event, board){
 			$(`#${event.target.id}`).text("O");
 		}
 		$(`#${event.target.id}`).removeClass("fadeText");
+		checkVictory(board);
 	}
 }
 var hoverIn = function(event, board){
@@ -72,5 +94,36 @@ var hoverOut = function(event, board){
 			$(`#${event.target.id}`).text(" ");	
 		}
 		$(`#${event.target.id}`).removeClass("fadeText");
+	}
+}
+
+var checkVictory = function(board){
+	if(board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][2] != " "){
+		alert(`End of Game, ${board[0][0]} won!`);
+		reset(board);
+	} else if(board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][2] != " "){
+		alert(`End of Game, ${board[1][0]} won!`);
+		reset(board);
+	} else if(board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][2] != " "){
+		alert(`End of Game, ${board[2][0]} won!`);
+		reset(board);
+	} else if(board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[2][0] != " "){
+		alert(`End of Game, ${board[0][0]} won!`);
+		reset(board);
+	} else if(board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[2][1] != " "){
+		alert(`End of Game, ${board[0][1]} won!`);
+		reset(board);
+	} else if(board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[2][2] != " "){
+		alert(`End of Game, ${board[0][2]} won!`);
+		reset(board);
+	}else if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[2][2] != " "){
+		alert(`End of Game, ${board[0][0]} won!`);
+		reset(board);
+	}else if(board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[2][0] != " "){
+		alert(`End of Game, ${board[0][2]} won!`);
+		reset(board);
+	} else if($.inArray(" ", board[0]) == -1 && $.inArray(" ", board[1]) == -1 && $.inArray(" ", board[2]) == -1){
+		alert("End of Game, It's a draw!");
+		reset(board);
 	}
 }
