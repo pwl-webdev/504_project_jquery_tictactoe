acda907a175af80410372f3df21af5b64c42ed53
Pwl Webdev <pwl.webdev@gmail.com>
2016-10-03 19:40:24 +0200

    Board created

--- assets/scripts/tictactoe.js
+++ assets/scripts/tictactoe.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
 	console.log("ready");
+	var board = initBoard();
+	render(board);
 });
+
+var initBoard = function(){
+	b = [[" "," "," "],[" "," "," "],[" "," "," "]];
+	return b;
+}
+var render = function(board){
+	$('.container').empty();
+	for(var i=0; i < 3; i++){
+		for(var j=0; j < 3; j++){
+			$('.container').append(`<div class="cell" id="c${j}r${i}">${board[i][j]}</div>`);
+		}
+	}
+}
